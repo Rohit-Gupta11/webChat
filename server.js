3fc7d5dbcf2ed2777ae70e2e380b3424bda325a0
Rohit Gupta <grohit7711@gmail.com>
2021-07-08 18:47:38 +0530

    login and register page included

--- server.js
+++ server.js
@@ -5,9 +5,32 @@ const http = require('http')
 const server = http.createServer(myapp)
 const PORT = process.env.PORT || 4000
 
+
+myapp.set('views', './views') 
+myapp.set('view engine', 'ejs')
+
+myapp.use(express.static('public'))
+
 myapp.get('/', (req,res) => {
-    res.send('hello world ')
+    res.render('login')
 })
+
+myapp.get('/login', (req,res) => {
+    res.render('login')
+})
+
+myapp.post('/login', (req,res) => {
+    res.send('done')
+})
+
+myapp.get('/register', (req,res) => {
+    res.render('register')
+})
+
+myapp.post('/register', (req,res) => {
+    res.send('registering')
+})
+
 server.listen(PORT, () => {
     console.log(`listening at http://localhost:${PORT}`)
 })
