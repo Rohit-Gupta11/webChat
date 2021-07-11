f8dc5571c06eec06e2e83006e0a19e5c7dfa1b6b
Rohit Gupta <grohit7711@gmail.com>
2021-07-11 17:11:44 +0530

    userdash ui pratially done

--- public/js/chat-functions.js
+++ public/js/chat-functions.js
@@ -2,9 +2,33 @@
 const logoutBtn = document.getElementById('logout-btn')
 logoutBtn.addEventListener('click', () => {
     fetch('/auth/logout', {
-        method : 'DELETE',
+        method: 'DELETE',
     }).then(() => {
         console.log('logout')
         location.href = '/'
     })
 })
+
+//profile user
+const profileUser = document.querySelector('.profile-user')
+profileUser.innerText = 'Hi, Rohit!'
+
+const yourList = document.querySelector('.your-list')
+
+
+function populateYourChat() {
+    let markup = `
+        <div class="your-list-item">
+            <div class="avatar">
+                <p class="is-size-5">R</p>
+            </div>
+            <div class="avatar-description">
+                <h4 class="is-size-5">Rahul</h4>
+                <p class="is-size-6">I will talk to them later.</p>
+            </div>
+        `
+    const temp = document.createElement('div')
+    temp.className = 'your-list-item'
+    temp.innerHTML = markup
+    yourList.appendChild(temp)
+}
